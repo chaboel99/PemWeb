f0cabeb1f98969f6daeb403034e7eddc110e960c
chaboel99 <chandra.sanjaya@students.paramadina.ac.id>
2023-06-21 10:30:29 +0700

    first commit

---
+++ datapelaporanlaka/laka.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `login_laka` (
+    `id` int(11) NOT NULL,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `login_laka` (`id`, `username`, `password`) VALUES
+(1, 'chandra', 'admin111');
